df6b1f1461d7776df2f4db9847e04a91fc555fc9
DanTheMan <danielturner184@gmail.com>
2020-08-31 01:04:19 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,17 @@
 ## code your solution here. 
+require_relative '../lib/meowing_cat'
+require 'pry'
+
+class Cat 
+    attr_accessor :name
+
+    def name
+        @name = "Maru"
+    end
+
+
+    def meow
+        puts "meow!"
+    end
+
+end
